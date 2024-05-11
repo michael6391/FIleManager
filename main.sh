3853693c97871ea0ed4c9c630ea5d71ce3b0841a
mikey501 <michaelkim6391@gmail.com>
2024-05-12 00:29:05 +0900

    change obsidian pdf file destination

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -avh --delete --progress ~/Documents ~/Library/Mobile\ Documents/com~apple~CloudDocs/BackupDocuments
-rsync -avhm --progress --include='*.pdf' --include='*/' --exclude='*' ~/Documents/ ~/Library/Mobile\ Documents/com~apple~CloudDocs/Obsidian/BRAIN/PDF/Updates
+rsync -avhm --progress --include='*.pdf' --include='*/' --exclude='*' ~/Documents/ ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/BRAIN/PDF/Updates
